597086e9b5afda0daaa9aad8f1db3a2979472570
Ghimirevivek <stephengmre@gmail.com>
2023-04-09 10:42:33 +0530

    me

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,17 @@
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid'
 
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'add':
+      return [...state, { id: uuidv4(), name: action.name }]
+    case 'fuel':
+      if (action.setBalance && action.balance >= 50) {
+        action.setBalance(action.balance - 50)
+      }
+      return state
+    default:
+      return state
+  }
+}
 
-export default reducer;
+export default reducer
